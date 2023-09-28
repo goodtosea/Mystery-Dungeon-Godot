fc422aff58e1b8ab3f41c2ceeba419a9a9bacd24
goodtosea <lnalex2456@gmail.com>
2023-09-28 16:33:31 -0700

    thinking about how to generate the floor

---
+++ floor.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+# class_name floor;
+
+var width;
+var height;
+
+var seed;
+
+# Called when the node enters the scene tree for the first time.
+func _ready(seed: int = 1):
+	pass # Replace with function body.
+
+
+func setup_layout():
+	$Layout.
+		
+		
+		
+	
