5c9c0b00574b0b1cf1abd35f27e9e67cbfd6e331
goodtosea <lnalex2456@gmail.com>
2023-10-01 14:12:20 -0700

    added basic functionality for place rooms

--- floor.gd
+++ floor.gd
@@ -2,10 +2,11 @@ extends Area2D
 
 # class_name floor;
 
-@export var width := 56;
-@export var height := 32;
+@export var width := 56
+@export var height := 32
+@export var hard_border_width := 2
 
-@export var seed := 1;
+@export var seed := 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready(seed: int = 1) -> void:
@@ -32,6 +33,14 @@ func fill_with_walls() -> void:
 
 func place_rooms() -> void:
 	
+	var offset_x := randi_range(hard_border_width, width - hard_border_width)
+	var offset_y := randi_range(hard_border_width, height - hard_border_width)
+	
+	var room_size_x := randi_range(5, 10) # replace 10 with size of sector later
+	var room_size_y := randi_range(4, 10)
+	for x in room_size_x:
+		for y in room_size_y:
+			$Layout.set_cell(0, Vector2i(x + offset_x, y + offset_y), 0, Vector2i(13, 1), 0)
 
 
 # Debug for floor generation
