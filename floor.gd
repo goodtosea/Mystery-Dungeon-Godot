40acd2badec4876510e432b6790ece685ce0cdce
goodtosea <lnalex2456@gmail.com>
2023-09-30 19:36:43 -0700

    added fill floor function

--- floor.gd
+++ floor.gd
@@ -2,25 +2,29 @@ extends Area2D
 
 # class_name floor;
 
-var width;
-var height;
+@export var width := 56;
+@export var height := 32;
 
-var seed;
+@export var seed := 1;
 
 # Called when the node enters the scene tree for the first time.
 func _ready(seed: int = 1):
-	pass # Replace with function body.
+	setup_layout()
+
 
 
 func setup_layout():
-	$Layout.position = Vector2(0, 0)
-	$Layout.clear()
-	$Layout.resize(width, height)
-	$Layout.seed = seed
+	fill_with_walls()
+#	place_rooms()
+#	place_corridors()
+#	place_deadends()
+#	draw_border()
+#	place_exception_tiles()
+#	place_terrain_features()
 	
 	
-		
-		
-		
-	
+func fill_with_walls():
+	for x in width:
+		for y in height:
+			$Layout.set_cell(0, Vector2i(x, y), 0, Vector2i(1, 1), 0)
 
