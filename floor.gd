081e257005de90af800d229f364ca1b6f03acaf6
goodtosea <lnalex2456@gmail.com>
2023-09-30 19:41:44 -0700

    added debug event hotkey

--- floor.gd
+++ floor.gd
@@ -8,12 +8,12 @@ extends Area2D
 @export var seed := 1;
 
 # Called when the node enters the scene tree for the first time.
-func _ready(seed: int = 1):
+func _ready(seed: int = 1) -> void:
 	setup_layout()
 
 
 
-func setup_layout():
+func setup_layout() -> void:
 	fill_with_walls()
 #	place_rooms()
 #	place_corridors()
@@ -23,8 +23,14 @@ func setup_layout():
 #	place_terrain_features()
 	
 	
-func fill_with_walls():
+func fill_with_walls() -> void:
 	for x in width:
 		for y in height:
 			$Layout.set_cell(0, Vector2i(x, y), 0, Vector2i(1, 1), 0)
+			
+
+# Debug for floor generation
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("reload debug"):
+		get_tree().reload_current_scene()
 
